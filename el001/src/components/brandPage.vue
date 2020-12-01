e9987d758f9130dedb97b5e3d4bd435faa342a74
RdzH <236504286@qq.com>
2020-12-02 02:12:14 +0800

    0

--- el001/src/components/brandPage.vue
+++ el001/src/components/brandPage.vue
@@ -1,91 +1,122 @@
 <template>
-<div>
-  <div class="flexcontainer">
-    <div class="commandBar">
-      <el-button type="primary">新增</el-button>
-      <el-button type="danger">删除</el-button>
-      <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    </div>
-    <div class="tableHeader">
-      <div class="column1">行号</div>
-      <div class="column2">名称</div>
-      <div class="column3">Logo</div>
-      <div class="column4">首字母</div>
-      <div class="column5H">描述详情</div>
-      <div class="column6">操作</div>
-    </div>
-    <div class="datatable">
-      <div class="datarow" v-for="(item, index) in dataStage" :key="item.ID">
-        <div class="column1">{{ index + 1 }}</div>
-        <div class="column2">{{ item.Name }}</div>
-        <div class="column3">
-          <el-image
-            class="img"
-            v-show="hasImage(item.Logo)"
-            :src="toImageUri(item.Logo)"
-            :lazy="true"
-            :fit="'contain'"
-          ></el-image>
-        </div>
-        <div class="column4">{{ item.Initial }}</div>
-        <div class="column5">
-          <div class="desc">{{ item.Desc }}</div>
-        </div>
-        <div class="column6">
-          <el-button type="primary">编辑</el-button>
+  <div class="page">
+    <div class="main">
+      <div class="commandBar">
+        <el-button type="primary">新增</el-button>
+        <el-button type="danger">删除</el-button>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+      </div>
+      <div class="tableHeader">
+        <div class="column1">行号</div>
+        <div class="column2">名称</div>
+        <div class="column3">Logo</div>
+        <div class="column4">首字母</div>
+        <div class="column5H">描述详情</div>
+        <div class="column6">操作</div>
+      </div>
+      <div class="datatable">
+        <div class="datarow" v-for="(item, index) in dataStage" :key="item.ID">
+          <div class="column1">{{ index + 1 }}</div>
+          <div class="column2">{{ item.Name }}</div>
+          <div class="column3">
+            <el-image
+              class="img"
+              v-show="hasImage(item.Logo)"
+              :src="toImageUri(item.Logo)"
+              :lazy="true"
+              :fit="'contain'"
+            ></el-image>
+          </div>
+          <div class="column4">{{ item.Initial }}</div>
+          <div class="column5">
+            <div class="desc">{{ item.Desc }}</div>
+          </div>
+          <div class="column6">
+            <el-button type="primary" @click="itemClick(item)">编辑</el-button>
+          </div>
         </div>
       </div>
+      <div class="pagination">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="[25, 50, 75, 100]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="dataList.length"
+        >
+        </el-pagination>
+      </div>
     </div>
-    <div class="pagination">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage"
-        :page-sizes="[25, 50, 75, 100]"
-        :page-size="pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="dataList.length"
+
+    <!-- 编辑弹窗 -->
+    <div>
+      <el-dialog
+        title="品牌编辑"
+        :visible.sync="dialogVisible"
+        :show-close="false"
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
       >
-      </el-pagination>
+        <el-form ref="form" :model="form" label-width="120px">
+          <el-form-item label="品牌名称：">
+            <el-input v-model="currentRowVM.Name"></el-input>
+          </el-form-item>
+          <el-form-item label="首字母：">
+            <el-input v-model="currentRowVM.Initial"></el-input>
+          </el-form-item>
+          <el-form-item label="活动形式">
+            <el-input type="textarea" height="150px" v-model="currentRowVM.Desc"></el-input>
+          </el-form-item>
+
+        
+        </el-form>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogVisible = false"
+              >提 交</el-button
+            >
+          </span>
+      </el-dialog>
     </div>
   </div>
-
-  <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
-
-<el-dialog
-  title="品牌信息编辑"
-  :visible.sync="dialogVisible"
-  :show-close="false"
-  :close-on-click-modal="false"
-  :close-on-press-escape="false"
-  width="30%"> 
-  <div>
-  <span>这是一段信息</span>
-    <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogVisible = false">提 交</el-button>
-  </div>
-</el-dialog>
-
-</div>
-
 </template>
 
 <script>
 export default {
-  methods: {
-    click() {
-      console.log(arguments.callee);
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 25,
+      dataList: [],
+      dataStage: [],
+      dialogVisible: false,
+      currentRowVM: {},
+    };
+  },
+  watch: {
+    currentPage() {
+      this.refreshDataState();
     },
-    cellstyle() {
-      return "locacell";
+    pageSize() {
+      this.refreshDataState();
+    },
+  },
+  methods: {
+    itemClick(item) {
+      if (item == false) {
+        return;
+      }
+      this.dialogVisible = true;
+      let str = JSON.stringify(item);
+      this.currentRowVM = JSON.parse(str);
     },
     handleSizeChange(val) {
       this.pageSize = val;
-      this.refreshDataState();
     },
     handleCurrentChange(val) {
       this.currentPage = val;
-      this.refreshDataState();
     },
     refreshDataState() {
       this.dataStage.splice(0, this.dataStage.length);
@@ -114,15 +145,7 @@ export default {
       return Math.ceil(this.dataList.length / this.pageSize);
     },
   },
-  data() {
-    return {
-      currentPage: 1,
-      pageSize: 25,
-      dataList: [],
-      dataStage: [],
-        dialogVisible: false
-    };
-  },
+
   mounted() {
     let vm = this;
     fetch("http://47.99.206.102:8410/getdata?tablename=brand")
@@ -136,7 +159,10 @@ export default {
 </script>
 
 <style scoped>
-.flexcontainer {
+.page {
+  height: 100%;
+}
+.main {
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -226,3 +252,4 @@ export default {
   flex: 0 0 auto;
 }
 </style>
+
